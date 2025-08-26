a2a477cc62c7ad621b4f47e678d5a37d77db44f9
Darren Daniel <127184690+darren2812@users.noreply.github.com>
2025-08-26 11:44:25 -0700

    Add files via upload

---
+++ ForLoopsPractice.java
@@ -0,0 +1,11 @@
+public class ForLoopsPractice {
+    public static void main(String[] args) {
+        // A loop must have a loop control variable.
+        // A loop control variable must be initialized, checked, and changed.
+
+        // increments numbers 1 to 10
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
